1ca3dba8ee81595be090250b292fdd5a064e703d
Dylan504 <275916476@qq.com>
2018-03-09 17:52:25 +0800

    font

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './bed.png';
+import Icon from './icon.png';
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack', '你好！'], ' ');
     element.classList.add('hello');
 
     // 将图像添加到我们现有的 div。
